45e70c8ea62b549256a6fe76f2157f0863554d8c
Danny Mancheno <Danny.mancheno97@gmail.com>
2024-01-02 20:00:35 -0500

    Added mobile friendly status bar theme color

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -39,13 +39,24 @@ export default function App() {
                     top: 0,
                     width: "100%",
                     height:"100%",
-                    zIndex: 0
+                    zIndex: 0,
                 }}
             >
+                <defs>
+                    <linearGradient id="myGradient" gradientTransform="rotate(90)">
+                        <stop offset="45%" stopColor="rgb(136, 90, 242)" />
+                        <stop offset="55%" stopColor="rgb(122, 81, 219)" />
+                    </linearGradient>
+                    <linearGradient id='myGradient2' gradientTransform='rotate(90)'>
+                        <stop offset="75%" stopColor="rgb(136, 90, 242)" />
+                        <stop offset="25%" stopColor="rgb(122, 81, 219)" />
+                    </linearGradient>
+                </defs>
                 <path d='M434-22.68l11,837.36H-269s415-163,445-448S44-22.68,44-22.68Z' 
                     id='splash-background-path1'
+                    fill="url('#myGradient')"
                     style={{
-                        fill: 'rgba(96, 63, 175, 0.3)',
+                        opacity: 0.5,
                         animation: (useIntro) ? 'animatePath1 2s forwards ease-out' : 'none',
                         transform: (useIntro) ? 'translate(-200% , -300%)' : 'translate(-120%, -300%)'
                     }}
@@ -53,8 +64,9 @@ export default function App() {
                 
                 <path d="M244.33-97.52s444,322.52,0,947.85H479s96-715.17-26.67-956.25Z" 
                     id='splash-background-path2'
+                    fill="url('#myGradient2')"
                     style={{
-                        fill: 'rgb(77, 51, 138, 0.3)',    
+                        opacity: 0.4,
                         animation: (useIntro) ? 'animatePath2 2s forwards  ease-out' : '',
                         transform: (useIntro) ? 'translate(-360%, -400%)' : 'translate(-400%, -400%)'
                     }}
